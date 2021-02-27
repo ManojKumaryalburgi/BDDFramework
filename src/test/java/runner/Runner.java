adf5110de51815245728e5874ae2b3ae848b0d34
manoj <manojyalburgi@gmail.com>
2021-02-27 20:33:31 +0530

    BDD

---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,16 @@
+package runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = {".\\src\\test\\java\\features"},
+glue = {"stepDefinition","hook"},
+dryRun = false,
+//tags = "@SmokeTest and @Functional",
+plugin = {"pretty", "html:target/cucumberreport.html",
+		"json:target/cucumber.json"},
+monochrome = true
+		)
+public class Runner extends AbstractTestNGCucumberTests{
+	
+}
